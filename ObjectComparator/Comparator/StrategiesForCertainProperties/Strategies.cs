7ccab1025bb5921e500c507d74463c0c767736de
Valeraf23 <valeraf23@gmail.com>
2018-10-30 12:06:16 +0200

    change strategies

---
+++ ObjectComparator/Comparator/StrategiesForCertainProperties/Strategies.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text.RegularExpressions;
+using ObjectComparator.Comparator.Interfaces;
+
+namespace ObjectComparator.Comparator.StrategiesForCertainProperties
+{
+    /// <summary>
+    /// define a comparison strategy for certain properties
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class Strategies<T> : IEnumerable<KeyValuePair<string, ICompareValues>>
+    {
+        private readonly IDictionary<string, ICompareValues> _strategies = new Dictionary<string, ICompareValues>();
+
+        public Strategies<T> Set<T1>(Expression<Func<T, T1>> targetAccessor,
+            Expression<Func<T1, T1, bool>> sourceValue) => Set(targetAccessor, sourceValue, new Display());
+
+        public Strategies<T> Set<T1>(Expression<Func<T, T1>> targetAccessor,
+            Expression<Func<T1, T1, bool>> sourceValue, Display distinctionRule)
+        {
+            var targetPropertyInfo = ToPropertyInfo(targetAccessor);
+            if (_strategies.ContainsKey(targetPropertyInfo))
+            {
+                throw new KeyNotFoundException($"Strategy for \"{targetPropertyInfo}\" has already contain");
+
+            }
+
+            _strategies.Add(targetPropertyInfo, new MemberStrategy<T1>(sourceValue, distinctionRule));
+
+            return this;
+        }
+
+        private static string ToPropertyInfo(LambdaExpression expression)
+        {
+            //Foo.name => name
+            //all after first dot Foo.name.second => name.second
+            var reg = new Regex(@"(?<=\.).*");
+            var match = reg.Match(expression.ToString());
+            var propName = match.Success ? match.Value : throw new Exception($"We could not parse {expression}");
+            return propName;
+        }
+
+        public IEnumerator<KeyValuePair<string, ICompareValues>> GetEnumerator()
+        {
+            return _strategies.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
