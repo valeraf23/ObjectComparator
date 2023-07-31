4b1acff4892f90803f9a95635aef54109eb81a03
Valerii Fedorenko <vfedorenko@vpn.mmsi.com>
2023-07-31 22:50:59 +0300

    add IDictionary<> support

---
+++ ObjectComparator.Tests/TestModels/StringDictionary.cs
@@ -0,0 +1,37 @@
+﻿    using System.Collections;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+
+    namespace ObjectsComparator.Tests.TestModels; 
+
+    internal class StringDictionary : IDictionary<string, string>
+    {
+        private readonly Dictionary<string, string> _dictionary = new();
+
+        public string this[string key]
+        {
+            get => _dictionary[key];
+            set => _dictionary[key] = value;
+        }
+
+        public int Count => _dictionary.Count;
+
+        public bool TryGetValue(string key, [MaybeNullWhen(false)] out string value) => _dictionary.TryGetValue(key, out value);
+
+        public IEnumerator<KeyValuePair<string, string>> GetEnumerator() => _dictionary.GetEnumerator();
+
+        public void Add(string key, string value) => _dictionary.Add(key, value);
+
+        public ICollection<string> Keys => throw new System.NotImplementedException();
+        public ICollection<string> Values => throw new System.NotImplementedException();
+        public bool IsReadOnly => throw new System.NotImplementedException();
+
+        public void Add(KeyValuePair<string, string> item) => throw new System.NotImplementedException();
+        public void Clear() => throw new System.NotImplementedException();
+        public bool Contains(KeyValuePair<string, string> item) => throw new System.NotImplementedException();
+        public bool ContainsKey(string key) => throw new System.NotImplementedException();
+        public void CopyTo(KeyValuePair<string, string>[] array, int arrayIndex) => throw new System.NotImplementedException();
+        public bool Remove(string key) => throw new System.NotImplementedException();
+        public bool Remove(KeyValuePair<string, string> item) => throw new System.NotImplementedException();
+        IEnumerator IEnumerable.GetEnumerator() => throw new System.NotImplementedException();
+    }
--- ObjectComparator/Comparator/Strategies/Implementations/Collections/DictionaryCompareStrategy.cs
+++ ObjectComparator/Comparator/Strategies/Implementations/Collections/DictionaryCompareStrategy.cs
@@ -1,47 +1,61 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using ObjectsComparator.Comparator.RepresentationDistinction;
 
-namespace ObjectsComparator.Comparator.Strategies.Implementations.Collections
+namespace ObjectsComparator.Comparator.Strategies.Implementations.Collections;
+
+public class DictionaryCompareStrategy : BaseCollectionsCompareStrategy
 {
-    public class DictionaryCompareStrategy : BaseCollectionsCompareStrategy
+    private static readonly MethodInfo CompareMethod = typeof(DictionaryCompareStrategy).GetTypeInfo()
+        .GetDeclaredMethod(nameof(CompareDictionary))!;
+
+    private static readonly Type DictionaryType = typeof(IDictionary<,>);
+
+    public DictionaryCompareStrategy(Comparator comparator) : base(comparator)
     {
-        private static readonly MethodInfo CompareMethod = typeof(DictionaryCompareStrategy).GetTypeInfo()
-            .GetDeclaredMethod(nameof(CompareDictionary))!;
+    }
 
-        public DictionaryCompareStrategy(Comparator comparator) : base(comparator)
+    public DeepEqualityResult CompareDictionary<TKey, TValue>(IDictionary<TKey, TValue> expected,
+        IDictionary<TKey, TValue> actual, string propertyName) where TKey : notnull
+    {
+        if (expected.Count != actual.Count)
+            return DeepEqualityResult.Create("Dictionary has different length", expected.Count, actual.Count);
+        var diff = DeepEqualityResult.Create();
+        foreach (var (key, value) in expected)
         {
+            if (!actual.TryGetValue(key, out var secondValue))
+                diff.Add(new Distinction(key.ToString()!, "Should be", "Does not exist"));
+
+            var diffRes = RulesHandler.GetFor(typeof(TValue))
+                .Compare(value, secondValue, $"{propertyName}[{key}]");
+
+            diff.AddRange(diffRes);
         }
 
-        public DeepEqualityResult CompareDictionary<TKey, TValue>(Dictionary<TKey, TValue> expected,
-            Dictionary<TKey, TValue> actual, string propertyName) where TKey : notnull
-        {
-            if (expected.Count != actual.Count)
-                return DeepEqualityResult.Create("Dictionary has different length", expected.Count, actual.Count);
-            var diff = DeepEqualityResult.Create();
-            foreach (var (key, value) in expected)
-            {
-                if (!actual.TryGetValue(key, out var secondValue))
-                    diff.Add(new Distinction(key.ToString(), "Should be", "Does not exist"));
+        return diff;
+    }
 
-                var diffRes = RulesHandler.GetFor(typeof(TValue))
-                    .Compare(value, secondValue, $"{propertyName}[{key}]");
+    public override DeepEqualityResult Compare<T>(T expected, T actual, string propertyName)
+    {
+        var genericArguments = GetGenericArguments(typeof(T));
+        return (DeepEqualityResult)CompareMethod.MakeGenericMethod(genericArguments)
+            .Invoke(this, new[] { (object)expected, actual, propertyName })!;
+    }
 
-                diff.AddRange(diffRes);
-            }
+    private static Type[] GetGenericArguments(Type type)
+    {
+        return type.GetInterfaces().Prepend(type)
+            .Where(i => i.IsGenericType && i.GetGenericTypeDefinition() == DictionaryType)
+            .Select(i => i.GetGenericArguments()).First();
+    }
 
-            return diff;
-        }
+    public override bool IsValid(Type member)
+    {
+        if (member.IsGenericType && member.GetGenericTypeDefinition() == DictionaryType) return true;
 
-        public override DeepEqualityResult Compare<T>(T expected, T actual, string propertyName)
-        {
-            var genericArguments = expected.GetType().GetGenericArguments();
-            return (DeepEqualityResult) CompareMethod.MakeGenericMethod(genericArguments)
-                .Invoke(this, new[] {(object) expected, actual, propertyName})!;
-        }
-
-        public override bool IsValid(Type member) =>
-            member.IsGenericType && member.GetGenericTypeDefinition() == typeof(Dictionary<,>);
+        return member.GetInterfaces().Any(interfaceType =>
+            interfaceType.IsGenericType && interfaceType.GetGenericTypeDefinition() == DictionaryType);
     }
 }
