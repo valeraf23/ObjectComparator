473f8507cd21e180f03b333e1ba5ac0075bdc324
Valerii Fedorenko <vfedorenko@vpn.mmsi.com>
2023-09-16 12:16:03 +0300

    3.5.4

---
+++ ObjectComparator/Comparator/Strategies/StrategiesForCertainProperties/MemberStrategyBase.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace ObjectsComparator.Comparator.Strategies.StrategiesForCertainProperties;
+
+public class MemberStrategyBase
+{
+    protected static readonly ConcurrentDictionary<string, Delegate> Cache = new();
+}
