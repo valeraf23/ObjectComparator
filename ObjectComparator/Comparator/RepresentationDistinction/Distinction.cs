f91942bb59b8a30e6f48807621f807e6a7619eae
Valerii Fedorenko <vfedorenko@vpn.mmsi.com>
2023-10-09 18:21:35 +0300

    fix issue 14

--- ObjectComparator/Comparator/RepresentationDistinction/Distinction.cs
+++ ObjectComparator/Comparator/RepresentationDistinction/Distinction.cs
@@ -1,48 +1,52 @@
 ﻿using ObjectsComparator.Helpers.GuardArgument;
 using System;
 
-namespace ObjectsComparator.Comparator.RepresentationDistinction
+namespace ObjectsComparator.Comparator.RepresentationDistinction;
+
+public class Distinction : IEquatable<Distinction>
 {
-    public class Distinction : IEquatable<Distinction>
+    public Distinction(string path, object? expectedValue, object? actualValue)
     {
-        public Distinction(string path, object? expectedValue, object? actuallyValue)
-        {
-            GuardArgument.ArgumentIsNotNull(path, $"{nameof(path)} can not be null or empty");
-            Path = path;
-            ExpectedValue = expectedValue;
-            ActuallyValue = actuallyValue;
-            Details = string.Empty;
-        }
-
-        public Distinction(string path, object expectedValue, object actuallyValue, string details) : this(path,
-            expectedValue, actuallyValue) =>
-            Details = details;
-
-        public string Path { get; }
-        public string Details { get; }
-        public object? ExpectedValue { get; }
-        public object? ActuallyValue { get; }
-
-        public bool Equals(Distinction? other) =>
-            other is not null &&
-            (ReferenceEquals(this, other) ||
-             Path.Equals(other.Path) && Details.Equals(other.Details));
-
-        public override string ToString()
-        {
-            var info = $"Path: \"{Path}\":\nExpected Value :{ExpectedValue}\nActually Value :{ActuallyValue}";
-            return string.IsNullOrEmpty(Details) ? info : $"{info}\n{nameof(Details)} : {Details}";
-        }
-
-        public override bool Equals(object? obj) => Equals(obj as Distinction);
-
-        public override int GetHashCode() => HashCode.Combine(Path, Details);
-
-        public static bool operator ==(Distinction? a, Distinction? b) =>
-            a is null && b is null ||
-            a?.Equals(b) == true;
-
-        public static bool operator !=(Distinction a, Distinction b) =>
-            !(a == b);
+        GuardArgument.ArgumentIsNotNull(path, $"{nameof(path)} cannot be null or empty");
+            
+        Path = path;
+        ExpectedValue = expectedValue;
+        ActualValue = actualValue;
+        Details = string.Empty;
     }
+
+    public Distinction(string path, object expectedValue, object actualValue, string details) 
+        : this(path, expectedValue, actualValue) => 
+        Details = details;
+
+    public string Path { get; }
+    public string Details { get; }
+    public object? ExpectedValue { get; }
+    public object? ActualValue { get; }
+
+    public override bool Equals(object? obj) => Equals(obj as Distinction);
+
+    public bool Equals(Distinction? other) =>
+        other != null && 
+        (ReferenceEquals(this, other) || StructuralEquality(other));
+
+    private bool StructuralEquality(Distinction other)
+    {
+        return Path == other.Path
+               && Details == other.Details
+               && Equals(ExpectedValue, other.ExpectedValue)
+               && Equals(ActualValue, other.ActualValue);
+    }
+
+    public override int GetHashCode() => HashCode.Combine(Path, Details, ExpectedValue, ActualValue);
+
+    public override string ToString()
+    {
+        var info = $"Path: \"{Path}\":\nExpected Value: {ExpectedValue}\nActual Value: {ActualValue}";
+        return string.IsNullOrEmpty(Details) ? info : $"{info}\n{nameof(Details)}: {Details}";
+    }
+
+    public static bool operator ==(Distinction? left, Distinction? right) => Equals(left, right);
+
+    public static bool operator !=(Distinction? left, Distinction? right) => !Equals(left, right);
 }
